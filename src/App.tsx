4ee8d9c378f3f1d5845057b0362808c44ebed39a
claire freehafer <22858381+clairefreehafer@users.noreply.github.com>
2024-03-11 13:50:54 -0700

    [MAIN] search input state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 
+type Package = {
+  name: string;
+  description: string;
+  npmLink: string;
+}
+
 function App() {
+  const [searchValue, setSearchValue] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [results, setResults] = useState<Package[]>([]);
+  const [error, setError] = useState("");
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setSearchValue(event.target.value)
+  }
+
+  function handleSearch(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log(searchValue);
+
+    // API call, setIsLoading
+  }
+
   return (
     <div className="app">
       <header className="header">
         <h1>npm</h1>
-        <form className="search">
-          <input type="search" className="search-input" />
+        <form className="search" onSubmit={handleSearch}>
+          <input
+            type="search"
+            className="search-input"
+            value={searchValue}
+            onChange={handleChange}
+          />
           <button type="submit">Search</button>
         </form>
       </header>
       <main>
-        <div>Loading...</div>
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          <ol>
+            <li>result</li>
+            <li>result</li>
+            <li>result</li>
+          </ol>
+        )}
 
-        <ol>
-          <li>result</li>
-          <li>result</li>
-          <li>result</li>
-        </ol>
+        {error && <div>{error}</div>}
       </main>
     </div>
   );
